13f34ccf60cbb8d2791a70dac48307535594909f
Tinu205 <dazleroger404@gmail.com>
2026-04-17 21:32:07 +0530

    Added for vidoes too

---
+++ build/CMakeFiles/ascii.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ascii.dir/ascii_video.cpp.o"
+  "CMakeFiles/ascii.dir/ascii_video.cpp.o.d"
+  "ascii"
+  "ascii.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ascii.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
